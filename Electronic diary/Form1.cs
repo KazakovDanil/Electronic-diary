e61cf577b50d3c0e22e2c21c4c1b6fa47a78c3aa
Kazak_oqfcdqr <kazakovdanya@gmail.com>
2024-03-27 23:45:38 +0300

    Добавьте файлы проекта.

---
+++ Electronic diary/Form1.cs
@@ -0,0 +1,15 @@
+namespace Electronic_diary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
